b6cdc14ad9793312b6eab5f76cb07f802ca0c905
CarolineFs <dr_gromova1998@mail.ru>
2016-11-22 14:32:40 +0300

    Create Зашифровать_слово.py

---
+++ Зашифровать_слово.py
@@ -0,0 +1,19 @@
+word = input ('Введите слово, которое нужно зашифровать')
+key = input ('Введите ключ')
+vocab = 'abcdefghigklmnopqrstuvwxyz'
+table = []
+for i in range (len(vocab)):
+    table.append(vocab[:i] + vocab [i:])
+if len (key) < len (word):
+    whole = len (word)//len(key)
+    longkey = key * whole
+    longkey +=key[:(len(word) - len (longkey))]
+elif len (key) > len (word):
+    longkey = key [:len(word)]
+cipher = ''
+for i in range (len(word)):
+    row = vocab.index(longkey[i])
+    col = vocab.index(word[i])
+    print(longkey[i], word [i], row, col)
+    cipher += table [row] [col]
+print (cipher)

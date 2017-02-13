6ad3a76d4cd15987696d60f00db8d9e7d6c8b6e2
CarolineFs <dr_gromova1998@mail.ru>
2017-02-13 22:04:24 +0300

    Update most_freq_word.py

--- DICT/most_freq_word.py
+++ DICT/most_freq_word.py
@@ -1,6 +1,7 @@
 #Программа должна прочитать текст, создать частотный список слов в этом тексте, 
 #используя слова в качестве ключей словаря, а частотность в качестве значений. 
 #После этого нужно распечатать самое частотное слово в тексте. 
+# Найти среднее значение частотности слов в тексте.
 with open ('anna.txt', 'r', encoding = 'utf - 8') as anna_file:
     text = anna_file.read()
     text = text.replace ('/n', ' ')
@@ -9,7 +10,8 @@ with open ('anna.txt', 'r', encoding = 'utf - 8') as anna_file:
     punct = ',()!?;:.<>}{[]|\/'
     words = [word.strip(punct) for word in words
              if word != '' or word != ' ']
-arr =[]
+summa = 0
+k = 0
 freq = {}
 for word in words:
     try:
@@ -19,13 +21,16 @@ for word in words:
 maximum = 0
 keys = list(freq.keys())
 values = list(freq.values())
-
 for i in values:
     if i > maximum:
         maximum = i;
+    summa += i
+    k+=1
 for key in keys:
     if freq[key] == maximum:
         
         print(key)
 print (maximum)
+aver_freq = summa%k
+print(aver_freq)
 

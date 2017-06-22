71709669208728722cf2b33eb94d3b20a34155bc
CarolineFs <dr_gromova1998@mail.ru>
2017-06-22 18:04:06 +0300

    Create words_xml_ana.py

---
+++ SOLVED/words_xml_ana.py
@@ -0,0 +1,46 @@
+#Посчитайте среднее количество разборов (тэг ana) на слово (тэг w).
+#Составьте частотный словарь всех частей речи в тексте. Например: {'APRO': 5, 'S': 277, ...}. Распечайте содержимое словаря в отдельный файл (на каждой строке "часть речи"<табуляция>"частотность").
+#Найдите в тексте все существительные в творительном падеже (обратите внимание, что некоторые разборы омонимичные. 
+#Если хотя бы один разбор с указанием творительного падежа присутствует, слово берём). 
+#Запишите в отдельный файл найденные существительные и контекст их употребления в таком формате:
+#3 слова слева<табуляция>найденное существительное<табуляция>3 слова справа.
+
+
+import re
+def openfile ():
+    with open ('text.xml', 'r', encoding = 'UTF-8') as f:
+        text = f.read()
+    return text
+def aver_ana_per_words ():
+    text2 = openfile()
+    ana = re.findall(r'<ana', text2)
+    ana = len(ana)
+    words = re.findall(r'<w>', text2)
+    words = len(words)
+    av = ana/words
+    return av
+def all_pos ():
+    text3 = openfile()
+    pos_dict = {}
+    pos = re.findall(r'gr="(\w+)', text3)
+    for i in pos:
+        if i not in pos_dict:
+            pos_dict[i] = 1
+        else:
+            pos_dict[i] += 1
+    
+    return pos_dict
+def writefile():
+    d = all_pos()
+    with open ('Parts of speech frequency', 'w', encoding = 'UTF-8') as f:
+        for pos in d:
+            f.writelines(pos+'\t'+str(d[pos])+'\n')
+print (all_pos())
+
+
+def main():
+    print (aver_ana_per_words ())
+    writefile()
+if __name__ == '__main__':
+    main()
+
